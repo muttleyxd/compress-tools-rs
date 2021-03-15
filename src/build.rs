adb78f590ce09cee172ef725d5893b2c53435c18
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2021-03-15 08:58:24 +0100

    Poor man's MinGW build

--- src/build.rs
+++ src/build.rs
@@ -6,17 +6,9 @@ fn main() {
     find_libarchive()
 }
 
-#[cfg(not(target_env = "msvc"))]
 fn find_libarchive() {
     pkg_config::Config::new()
         .atleast_version("3.2.0")
         .probe("libarchive")
-        .expect("Unable to find libarchive");
-}
-
-#[cfg(target_env = "msvc")]
-fn find_libarchive() {
-    vcpkg::Config::new()
-        .find_package("libarchive")
         .expect("Unable to find libarchive");
 }
